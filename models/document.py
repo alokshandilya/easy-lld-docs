83b3935840b86b63938dd3461d4e85e990f4443b
Alok Shandilya <alok.shandilya10@gmail.com>
2025-03-05 12:27:04 +0530

    refactor: add docstring documentation comment

--- models/document.py
+++ models/document.py
@@ -2,6 +2,15 @@ from datetime import datetime
 
 
 class Document:
+    """
+    This class represents a document.
+
+    Attributes:
+    - doc_id (int): The unique id of the document.
+    - owner_id (int): The user id of the owner of the document.
+    - content (str): The content of the document.
+    """
+
     def __init__(self, doc_id: int, owner_id: int, content: str):
         self.doc_id = doc_id
         self.owner_id = owner_id
--- models/user.py
+++ models/user.py
@@ -1,4 +1,12 @@
 class User:
+    """
+    User class to represent a user in the system.
+
+    Attributes:
+    - user_id (int): The unique id of the user.
+    - name (str): The name of the user.
+    """
+
     def __init__(self, user_id: int, name: str):
         self.user_id = user_id
         self.name = name
